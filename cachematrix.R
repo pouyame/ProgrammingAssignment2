3af5545a2d481fae5fb89175c78cbd65345069e6
pouyame <85084584+pouyame@users.noreply.github.com>
2021-05-31 14:56:38 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+      invr <- NULL    #inerse matrix
+      set <- function(y) {
+            mat <<- y     #Cached Matrix
+            invr <<- NULL #Cached inverse Matrix 'empty'
+      }
+      #recall matrix
+      get <- function() mat     
+      # register inverse matrix to cach
+      setinv <- function(inverse) invr <<- inverse  
+      # recall cached inverse matrix
+      getinv <- function() invr
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      invr <- x$getinv()  #recall hached matrix
+      if(!is.null(invr)) {
+            message("getting cached inverse")
+            return(invr)
+      }
+      mat <- x$get()  #recall hached matrix
+      #calculate inverse
+      invr <- solve(mat)
+      # record inverse natrix in hach
+      x$setinv(invr)
+      invr
 }
+      
+      
